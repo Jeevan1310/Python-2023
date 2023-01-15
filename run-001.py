e3be060e63d492c2dee6736a5d2429364879ae95
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2023-01-15 20:18:42 +0530

    step added

--- run-001.py
+++ run-001.py
@@ -22,3 +22,18 @@ def month_days(month, leap_year):
 name = input("input your name: ")
 age = input("input your age: ")
 localtime = time.localtime(time.time())
+
+year = int(age)
+month = year * 12 + localtime.tm_mon
+day = 0
+begin_year = int(localtime.tm_year) - year
+end_year = begin_year + year
+
+for y in range(begin_year, end_year):
+    if (judge_leap_year(y)):
+        day = day + 366
+    else:
+        day = day + 365
+leap_year = judge_leap_year(localtime.tm_year)
+for m in range(1, localtime.tm_mon):
+    day = day + month_days(m, leap_year)
